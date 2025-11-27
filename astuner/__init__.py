3b9b96ab7281b48bbf364bd3a94003eabe48935d
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2025-11-27 18:18:49 +0800

    add version number
    Former-commit-id: a7a73b04d11ea8e1facc1cef45842936027c7feb

--- astuner/__init__.py
+++ astuner/__init__.py
@@ -1,3 +1,12 @@
 from astuner.schema.task import WorkflowOutput, WorkflowTask
 from astuner.tuner import ModelTuner
 from astuner.workflow import Workflow
+
+__all__ = [
+    "Workflow",
+    "WorkflowTask",
+    "WorkflowOutput",
+    "ModelTuner",
+]
+
+__version__ = "0.1.0"
