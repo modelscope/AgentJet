3c444632172701c7f4810651ea2a24feea669df8
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2025-11-27 19:49:56 +0800

    fix trinity backbone
    Former-commit-id: 037592bb34aa20842ce6f9748fd9556f99765d54

--- astuner/backbone/__init__.py
+++ astuner/backbone/__init__.py
@@ -1,6 +1,16 @@
 from loguru import logger
 
 try:
-    pass
+    from astune.backbone.trinity_compat_workflow import (
+        ASTunerTaskReader,
+        ASTunetWorkflowWrap,
+        TrinityCompatWorkflow,
+    )
+
+    __all__ = [
+        "TrinityCompatWorkflow",
+        "ASTunetWorkflowWrap",
+        "ASTunerTaskReader",
+    ]
 except ImportError:
     logger.warning("trinity is not available.")
