8610b41d40e9ac2b3cd6f7606140c2164af162f6
cc <cc445845@alibaba-inc.com>
2025-12-19 16:43:15 +0800

    refactor: extract BaseAgentRunner to separate module

---
+++ astuner/task_runner/base_runner.py
@@ -0,0 +1,49 @@
+from typing import Any, Callable, Union
+
+from astuner.context_tracker.basic_tracker import BaseContextTracker
+from astuner.task_judge.base_judge import JudgeBase
+from astuner.utils.dynamic_import import dynamic_import
+from astuner.utils.utils import run_async_coroutine_with_timeout
+
+
+class BaseAgentRunner(object):
+    def __init__(self, llm_inference_fn: Callable, tokenizer: Any, config, **kwargs):
+        self.tokenizer = tokenizer
+        self.instruction_template_ids = self.tokenizer.encode("<|im_start|>user\n")
+        self.response_template_ids = self.tokenizer.encode("<|im_start|>assistant\n")
+        self.tracker: Union[BaseContextTracker, Any, None] = None
+        self.alien_llm_chat_fn: Union[Callable, None] = None
+        self.llm_inference_fn: Callable = llm_inference_fn
+        self.config = config
+        self.max_steps: int = self.config.astuner.rollout.multi_turn.max_steps
+        self.max_model_len: int = self.config.astuner.rollout.max_model_len
+        self.max_env_len: int = self.config.astuner.context_tracker.max_env_len
+
+    def get_judge(self) -> JudgeBase:  # type: ignore
+        if self.config.astuner.task_judge.judge_type == "customized_protocol":
+            judge_protocol = self.config.astuner.task_judge.judge_protocol
+            return dynamic_import(judge_protocol)(self.config)  # type: ignore
+
+        elif self.config.astuner.task_judge.judge_type == "rubrics_auto_grader":
+            # astuner/task_judge/rm_auto_grader_judge.py
+            from astuner.task_judge.rm_auto_grader_judge import RMAutoGraderJudge
+
+            judge = RMAutoGraderJudge(self.config)
+            run_async_coroutine_with_timeout(judge.load_rubrics_from_cache())
+            return judge
+
+    def runner_hooks(self, observation_window, task_thread_index, workflow_task):
+        def should_interrupt_fn() -> bool:
+            if (observation_window["stop"] is not None) and observation_window["stop"][
+                task_thread_index
+            ]:  # Check if the thread should stop (because other threads have completed, making this thread useless)
+                return True
+            return False
+
+        def generated_token_callback_fn(token_array):
+            observation_window["token"][task_thread_index] += len(token_array)
+
+        return {
+            "should_interrupt_fn": should_interrupt_fn,
+            "generated_token_callback_fn": generated_token_callback_fn,
+        }
