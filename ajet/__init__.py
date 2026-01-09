41cdc56d5cf05c36a62bc6073a4ee97b2ab64a0e
cc <cc445845@alibaba-inc.com>
2026-01-09 18:33:41 +0800

    fix: restore original code

--- ajet/__init__.py
+++ ajet/__init__.py
@@ -2,6 +2,7 @@ from ajet.copilot.job import AgentJetJob
 from ajet.schema.task import WorkflowOutput, WorkflowTask
 from ajet.tuner import AjetTuner
 from ajet.workflow import Workflow
+from ajet.utils.vsdb import vscode_conditional_breakpoint as bp
 
 __all__ = [
     "Workflow",
@@ -9,6 +10,7 @@ __all__ = [
     "WorkflowOutput",
     "AjetTuner",
     "AgentJetJob",
+    "bp"
 ]
 
 __version__ = "0.1.0"
